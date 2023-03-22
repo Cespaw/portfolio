2e5cd1c1ebe813969dad4c1ca0bcfb57ed178756
Cesar Aaron Alexander Pawlik <cesarp@hotmail.se>
2023-03-22 16:16:26 +0100

    Added toast when sending messages through contact

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,23 @@
 import React, { useRef } from 'react'
 import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Contact = (props) => {
 
     const form = useRef();
+
+    const showToastMessage = (status) => {
+        if (status) {
+            toast.success('Your message was sent!', {
+                position: toast.POSITION.BOTTOM_RIGHT
+            });
+        } else {
+            toast.error('Your message could not be sent', {
+                position: toast.POSITION.BOTTOM_RIGHT
+            });
+        }
+    };
 
     const sendEmail = (e) => {
 
@@ -14,9 +28,10 @@ const Contact = (props) => {
         emailjs.sendForm('service_yqzmzho', 'template_2extsur', form.current, 'FfVMct-IXN05keTvg')
             .then((result) => {
                 console.log(result.text);
-                console.log('Email sent!')
+                showToastMessage(true)
             }, (error) => {
                 console.log(error.text);
+                showToastMessage(false)
             });
     }
 
@@ -31,6 +46,7 @@ const Contact = (props) => {
                 <textarea className='textarea' name="message" maxLength={2000} value={props.message} onChange={e => props.setMessage(e.target.value)}></textarea>
                 <input type="submit" value="Send"></input>
             </form>
+            <ToastContainer />
         </section>
     )
 }
